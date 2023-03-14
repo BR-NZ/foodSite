351f17bb42ae16a515986937840f23d0f557656c
Alexey Beschetnikoff <bronzspawn@gmail.com>
2023-03-14 23:18:22 +0700

    Switched menu cards creation to class generated objects using template

--- js/script.js
+++ js/script.js
@@ -90,7 +90,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     setClock('.timer', deadline);
 
-    // Modal pop-up
+    // POPUP
 
     const btnsModal = document.querySelectorAll('*[data-modal]'),
           modalPopup = document.querySelector('.modal'),
@@ -122,7 +122,7 @@ window.addEventListener('DOMContentLoaded', () => {
         if(e.code === 'Escape' && modalPopup.classList.contains('show')) closeModal(modalPopup);
     });
 
-    const modalTimerID = setTimeout(() => showModal(modalPopup), 5000);
+    // const modalTimerID = setTimeout(() => showModal(modalPopup), 5000);
 
     function showModalOnScroll() {
         if(document.documentElement.scrollHeight === document.documentElement.clientHeight + window.pageYOffset) {
@@ -131,4 +131,65 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
     window.addEventListener('scroll', showModalOnScroll);
+
+    // CARDS
+
+    class MenuCard {
+        constructor(src, alt, title, desc, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.desc = desc;
+            this.price = price;
+            this.transfer = 27;
+            this.parent = document.querySelector(parentSelector);
+            this.changeToUAH();
+        }
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+        render() {
+            const menuItem = document.createElement('div');
+            menuItem.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.desc}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+            </div>
+            `;
+            this.parent.append(menuItem);
+        }
+    }
+
+    new MenuCard(
+        'img/tabs/vegy.jpg',
+        'vegy',
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        8,
+        '.menu__field .container'
+    ).render();
+
+    new MenuCard(
+        'img/tabs/elite.jpg',
+        'elite',
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        9,
+        '.menu__field .container'
+    ).render();
+
+    new MenuCard(
+        'img/tabs/post.jpg',
+        'post',
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        6,
+        '.menu__field .container'
+    ).render();
 });
